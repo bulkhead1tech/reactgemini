78d29bd56ccda19b6b5d07beae90f363ee883bd7
Prasang Charitra <150513432+bulkhead1tech@users.noreply.github.com>
2025-02-08 11:19:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <>
-    <div className='flex flex-col p-2 bg-black h-screen w-screen bg-gradient-to-t from-teal-950 backdrop-blur-lg via-transparent to-transparent'>
+    <div className='flex flex-col p-2 bg-black overflow-hidden h-screen w-screen bg-gradient-to-t from-teal-950 backdrop-blur-lg via-transparent to-transparent'>
      <div className='h-[10vh] w-full flex rounded-4xl shadow- justify-start bg-transparent px-5 -inset-2 backdrop-blur-3xl shadow-teal-800 shadow-lg text-teal-500 items-center'><h1 className='text-4xl font-bold'>M</h1><h4 className='text-xl text-white font-mono pt-2'>ichanikoi</h4></div>
      <div className='flex p-5 w-full h-full flex-col lg:w-full lg:h-full lg:flex-row'>
      <div className='h-1/2 w-full flex flex-col  justify-center px-5 py-10 items-center md:h-full md:w-1/2 lg:h-full lg:w-1/2 md:flex-row lg:flex-row'> 
@@ -49,7 +49,7 @@ function App() {
 </h1>
      </div>
      <div className='h-1/2 w-full flex flex-col md:h-full md:w-1/2 lg:h-full lg:w-1/2 lg:p-10  justify-center items-center'>
-     <div className='h-1/3 w-full px-5 py-1 text-red-500 flex items-center justify-center border-slate-400 border-2 border-solid text-4xl'>
+     <div className='h-1/3 w-full px-5 py-5 text-red-500 flex items-center justify-center border-slate-400 border-2 border-solid text-4xl'>
      {first!=""?<>{first}</>:<>{"Loading..."}</>}
      </div>
       <form className='h-fit w-fit p-2 flex flex-col gap-y-2' onSubmit={submithandler} encType='multipart/form-data'>
